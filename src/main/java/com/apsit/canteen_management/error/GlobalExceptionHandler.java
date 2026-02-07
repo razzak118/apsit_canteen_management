8c7d669ff1eadbed8c41cf0869520f837fed31ca
razzak118 <rajjakansari118@gmail.com>
2026-02-07 22:56:48 +0530

    add global exception handler

---
+++ src/main/java/com/apsit/canteen_management/error/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.apsit.canteen_management.error;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> jwtExceptionHandler(JwtException ex){
+        ApiError apiError=new ApiError("invalid Jwt as: "+ex.getMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ApiError> usernameNotFoundExceptionHandler(UsernameNotFoundException ex){
+        ApiError apiError=new ApiError("user not found with username: "+ ex.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> authenticationExceptionHandler(AuthenticationException ex){
+        ApiError apiError = new ApiError("Authentication failed as: "+ex.getMessage(), HttpStatus.UNAUTHORIZED);
+        return  new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> generalExceptionHandler(Exception ex){
+        ApiError apiError=new ApiError("Unexpected error occured: "+ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ApiError> expiredJwtExceptionHandler(ExpiredJwtException ex){
+        ApiError apiError=new ApiError("Jwt Expired !", HttpStatus.NON_AUTHORITATIVE_INFORMATION);
+        return new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ApiError> malformedJwtExceptionHandler(MalformedJwtException ex){
+        ApiError apiError=new ApiError("Invalid Jwt: "+ex.getMessage(), HttpStatus.NON_AUTHORITATIVE_INFORMATION);
+        return new ResponseEntity<>(apiError, apiError.getHttpStatus());
+    }
+}
--- src/main/java/com/apsit/canteen_management/security/JwtAuthFilter.java
+++ src/main/java/com/apsit/canteen_management/security/JwtAuthFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -23,27 +24,40 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
     private final AuthUtil authUtil;
 
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String requestTokenHeader= request.getHeader("Authorization");
-        if(requestTokenHeader==null || !requestTokenHeader.startsWith("Bearer")){
-            filterChain.doFilter(request, response);
-            return;
-        }
-        String token= requestTokenHeader.split("Bearer ")[1];
+        try {
+            final String requestTokenHeader = request.getHeader("Authorization");
+            if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = requestTokenHeader.split("Bearer ")[1];
         /*
          "Bearer" is used in Authorization as a key which tell that this is a token which user holds
          so that server knows which way do the client wants to authorize himself.
          Note-: since "Bearer " is just a key not the part of actual token and space is seperator, we have to extract
          the legit token form it. So that we can either split the key and token or just take substring like token.substring(8)
          */
-        String username= authUtil.getUsernameFromToken(token);
-        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-            User user= userRepository.findByUsername(username).orElseThrow();
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=
-                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            String username = authUtil.getUsernameFromToken(token);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                User user = userRepository.findByUsername(username).orElseThrow();
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+            filterChain.doFilter(request, response);
+        }catch (Exception e){
+            handlerExceptionResolver.resolveException(request, response, null, e);
+            /* although we made exception handler in global exception handler but still we won't be able to use them
+             So here is the concept of layering spring boot.
+             the global exception handler handles only the exceptions which are thrown from our controller layer
+             But since JwtAuthFilter is a filter therefore it throws error way before getting into the controller layer
+             where spring actrually dones'nt know how to handle it therefore we manualy push the excpetion to out
+             global exception handler using HandlerExceptionResolver.
+             */
         }
-        filterChain.doFilter(request, response);
     }
 }
