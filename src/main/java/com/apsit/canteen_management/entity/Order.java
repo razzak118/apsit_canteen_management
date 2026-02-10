de55d481d57e7568cdc7ec271333030abf409b10
razzak118 <rajjakansari118@gmail.com>
2026-02-10 20:07:24 +0530

    add order entity

---
+++ src/main/java/com/apsit/canteen_management/entity/Order.java
@@ -0,0 +1,14 @@
+package com.apsit.canteen_management.entity;
+
+import com.apsit.canteen_management.enums.OrderStatus;
+import jakarta.persistence.*;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+}
---
+++ src/main/java/com/apsit/canteen_management/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.apsit.canteen_management.enums;
+
+public enum OrderStatus {
+    CONFIRMED,
+    PENDING,
+    CANCELLED
+}
